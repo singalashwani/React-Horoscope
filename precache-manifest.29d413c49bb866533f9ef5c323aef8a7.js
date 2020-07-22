af00115a31f79eb07725687253315a602ee6b94c
Ashwani Singal <singalashwani@rediffmail.com>
2020-07-22 17:46:20 +0530

    Updates

---
+++ precache-manifest.29d413c49bb866533f9ef5c323aef8a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec162694844f9e04d2f4b8033a8df7ab",
+    "url": "/React-Horoscope/index.html"
+  },
+  {
+    "revision": "7b350484f391dff1668c",
+    "url": "/React-Horoscope/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "adf4a6595c51c86ba349",
+    "url": "/React-Horoscope/static/css/main.cc9014e3.chunk.css"
+  },
+  {
+    "revision": "7b350484f391dff1668c",
+    "url": "/React-Horoscope/static/js/2.adb471dd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Horoscope/static/js/2.adb471dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adf4a6595c51c86ba349",
+    "url": "/React-Horoscope/static/js/main.f47815bb.chunk.js"
+  },
+  {
+    "revision": "cb9be77107295025c5d8",
+    "url": "/React-Horoscope/static/js/runtime-main.59f33275.js"
+  }
+]);
